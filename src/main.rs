ab5ac9adeab0da0f9d203b63abac57ce37dfa51c
Maaz Khokhar <khokharmaaz@gmail.com>
2026-09-01 20:47:08 -0500

    panic handler actually shows panicing

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello, world!");
-
-    loop {}
+    panic!("Hello!");
 }
